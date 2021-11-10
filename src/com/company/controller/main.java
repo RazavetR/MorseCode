ef14a3b5215344e27e3bd19b6de3ccb47b3b058d
RazavetR <razavet.r@gmail.com>
2021-11-10 15:10:31 -0500

    Completing classes

---
+++ src/com/company/controller/main.java
@@ -0,0 +1,11 @@
+package com.company.controller;
+
+public class main {
+    public static void main(String[] args) {
+        String toTranslate = "";
+        System.out.println("Please enter a sentence to be translated : ");
+
+
+
+    }
+}
---
+++ src/com/company/morse/Morse.java
@@ -0,0 +1,15 @@
+package com.company.morse;
+
+public class Morse {
+    private ShortSymbol shorts;
+    private LongSymbol longs;
+
+    public Morse() {
+        shorts = new ShortSymbol();
+        longs = new LongSymbol();
+    }
+
+    public String translate(String toTranslate) {
+        return "";
+    }
+}
